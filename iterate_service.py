8624ca9ab3c6f1b29e38dbbebf34a155d0beb7c2
Robert Greinacher <robert@3antworten.de>
2016-10-26 13:10:46 +0200

    converter service & nltk playground

---
+++ iterate_service.py
@@ -0,0 +1,131 @@
+#!/usr/local/bin/python3.5
+# -*- coding: utf-8 -*-
+
+# import python libs
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from pprint import pprint as pp
+import argparse
+import base64
+import json
+import logging
+import socket
+
+# defining globals & constants
+
+INTERFACE_TYPE = 'ner_complete'
+app = Flask(__name__)
+
+# Flask routes
+
+@app.route('/', methods=['GET'])
+@app.route('/iterate', methods=['GET'])
+@app.route('/who_are_you', methods=['GET'])
+def who_are_you():
+    message = {
+        'role': 'iterate',
+        'title': 'RawData Converter',
+        'description': 'Iterate service dummy to convert RawData to renderable AnnotationDocuments.',
+        'version': 0.1,
+        'problem_id': 'ner',
+        'interface_types': [ INTERFACE_TYPE ]
+    }
+    return create_json_respons_from(message)
+
+@app.route('/iterate', methods=['POST'])
+def iterate():
+    app.logger.info('post iterate')
+    post_json_data = json.dumps(request.json)
+    corpus = decode_post_data(post_json_data)
+    pp(post_json_data)
+    documents = create_annotation_documents(corpus)
+
+    app.logger.info('created annotation documents from posted JSON data')
+    app.logger.info(documents)
+
+    response = { 'annotation_documents': documents }
+    return create_json_respons_from(response)
+
+# helpers
+
+def decode_post_data(post_json_data):
+    dict_content = json.JSONDecoder().decode(post_json_data)
+    for raw_datum in dict_content:
+        encoded_content = raw_datum['content']
+        deconded_content = base64.b64decode(encoded_content).decode('utf-8')
+        raw_datum['content'] = deconded_content
+
+    return dict_content
+
+def create_annotation_documents(corpus):
+    annotation_documents = []
+    for index, corpus_part in enumerate(corpus):
+        raw_datum_id = corpus_part['raw_datum_id']
+        content = corpus_part['content']
+        parsed_content = json.JSONDecoder().decode(content)
+        payload = parsed_content['content']
+
+        document = {
+            'rank': index,
+            'raw_datum_id': raw_datum_id,
+            'payload': payload,
+            'interface_type': INTERFACE_TYPE
+        }
+        annotation_documents.append(document)
+
+    return annotation_documents
+
+def create_json_respons_from(hash):
+    response = jsonify(hash)
+    response.status_code = 200
+    return response
+
+# entry point as a stand alone script
+
+if __name__ == '__main__':
+    beVerbose = False
+    usePort = 5001
+    useHost = 'localhost'
+    parser = argparse.ArgumentParser(
+        description='Dalphi Iterate Service; 13.10.16 Robert Greinacher')
+
+    parser.add_argument(
+        '-p',
+        '--port',
+        type=int,
+        help='set the network port number')
+    parser.add_argument(
+        '-l',
+        '--localhost',
+        action='store_true',
+        dest='localhost',
+        help='use "localhost" instead of current network IP')
+    parser.add_argument(
+        '-d',
+        '--daemon',
+        action='store_true',
+        dest='daemon',
+        help='enables daemon mode')
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        dest='verbose',
+        help='enables verbose mode')
+    args = parser.parse_args()
+
+    if args.port:
+        usePort = args.port
+    if not args.localhost:
+        hostename = socket.gethostname()
+        useHost = socket.gethostbyname(hostename)
+    if args.verbose:
+        beVerbose = True
+
+    file_handler = logging.FileHandler('service.log')
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('start running flask app')
+    app.run(useHost, usePort, beVerbose)
---
+++ nltk_nec.py
@@ -0,0 +1,90 @@
+import nltk
+from nltk import Tree
+from nltk.draw.util import CanvasFrame
+from nltk.draw import TreeWidget
+from pprint import pprint as pp
+
+name_index = 0
+text_en = "In the third category he included those Brothers (the majority) who saw nothing in Freemasonry but the external forms and ceremonies, and prized the strict performance of these forms without troubling about their purport or significance. Such were Willarski and even the Grand Master of the principal lodge. Finally, to the fourth category also a great many Brothers belonged, particularly those who had lately joined. These according to Pierre's observations were men who had no belief in anything, nor desire for anything, but joined the Freemasons merely to associate with the wealthy young Brothers who were influential through their connections or rank, and of whom there were very many in the lodge.Pierre began to feel dissatisfied with what he was doing. Freemasonry, at any rate as he saw it here, sometimes seemed to him based merely on externals. He did not think of doubting Freemasonry itself, but suspected that Russian Masonry had taken a wrong path and deviated from its original principles. And so toward the end of the year he went abroad to be initiated into the higher secrets of the order.What is to be done in these circumstances? To favor revolutions, overthrow everything, repel force by force?No! We are very far from that. Every violent reform deserves censure, for it quite fails to remedy evil while men remain what they are, and also because wisdom needs no violence. \"But what is there in running across it like that?\" said Ilagin's groom. \"Once she had missed it and turned it away, any mongrel could take it,\" Ilagin was saying at the same time, breathless from his gallop and his excitement."
+text_de = "In fünf Bundesländern haben Spezialeinheiten der Polizei am Dienstag Wohnungen wegen des Verdachts auf Terror-Planungen durchsucht. Einsatzkommandos gingen nach Angaben des Landeskriminalamtes in Erfurt in zwölf Wohnungen und einer Gemeinschaftsunterkunft in Thüringen, Hamburg, Nordrhein-Westfalen, Sachsen und Bayern vor. Anlass der Ermittlungen war nach den Angaben der Verdacht der Vorbereitung einer schweren staatsgefährdenden Gewalttat. Eine konkrete Anschlagsgefahr gab es nach den bisherigen Ermittlungen nicht. Zu Festnahmen ist es nach Auskunft der Polizei nicht gekommen. „Es gibt keinen Haftbefehl“, sagte eine Sprecherin des Thüringer Landeskriminalamtes am Dienstagnachmittag in Erfurt. Hauptverdächtig sei ein 28 Jahre alter Tschetschene mit russischem Pass, der als Asylbewerber im thüringischen Suhl leben soll."
+text_en = 'I am very excited about the next generation of Apple products.'
+text_en = 'I hate Apple products'
+
+def ner_pipeline(raw_text):
+    # create a list of strings
+    print('sentence splitting ...')
+    sentences = sentence_splitting(raw_text)
+
+    # create a list of lists of strings
+    print('word tokenizing ...')
+    tokenized_sentences = [word_tokenization(sentence) for sentence in sentences]
+
+    # create a list of lists of tuples
+    print('pos tagging ...')
+    pos_tagged_sentences = [part_of_speech_tagging(sentence) for sentence in tokenized_sentences]
+
+    # create a list of nltk trees containing named entity chunks
+    print('named entity token chunking ...')
+    chunk_trees = [named_entity_token_chunking(sentence) for sentence in pos_tagged_sentences]
+
+    return chunk_trees
+
+def sentence_splitting(raw_text):
+    return nltk.sent_tokenize(raw_text)
+
+def word_tokenization(sentence):
+    return nltk.word_tokenize(sentence)
+
+def part_of_speech_tagging(sentence):
+    return nltk.pos_tag(sentence)
+
+def named_entity_token_chunking(tagged_sentence, report_ne=True):
+    # return nltk.ne_chunk(tagged_sentence)
+
+    # Loads the serialized NEChunkParser object
+    chunker = nltk.data.load('chunkers/maxent_ne_chunker/PY3/english_ace_multiclass.pickle')
+
+    # The MaxEnt classifier
+    maxEnt = chunker._tagger.classifier()
+
+    tags = []
+    for i in range(0, len(tagged_sentence)):
+        featureset = chunker._tagger.feature_detector(tagged_sentence, i, tags)
+        tag = chunker._tagger.choose_tag(tagged_sentence, i, tags)
+        # if report_ne and tag != 'O':
+        if report_ne:
+            print('\nExplanation on the why the word \'' + tagged_sentence[i][0] + '\' was tagged:')
+            featureset = chunker._tagger.feature_detector(tagged_sentence, i, tags)
+            maxEnt.explain(featureset)
+        tags.append(tag)
+
+    return tags
+
+# helper
+
+def draw_to_file(tree):
+    global name_index
+
+    canvas = CanvasFrame()
+    tree_canvas = TreeWidget(canvas.canvas(), tree)
+    canvas.add_widget(tree_canvas,10,10)
+
+    name_index += 1
+    file_name = 'tree_plot_' + str(name_index) + '.ps'
+
+    canvas.print_to_file(file_name)
+    canvas.destroy()
+
+    # clean up using the following shell comand:
+    # for i in tree_plot_*.ps; do convert $i "$i.png"; rm $i; done
+
+# entry point as a stand alone script
+
+if __name__ == '__main__':
+    chunk_trees = ner_pipeline(text_en)
+    # chunk_trees = ner_pipeline(text_de)
+    pp(chunk_trees)
+
+    # read on:
+    # http://www.nltk.org/book/ch07.html#sec-ner chaper 3
+    # http://mattshomepage.com/articles/2016/May/23/nltk_nec/
