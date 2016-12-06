a5e4ec42f1b645795ba8a8a60c4b2b448f90df62
Arik Grahl <mail@arik-grahl.de>
2016-12-06 18:12:17 +0100

    implement API changes for asynchronous communication

--- iterate_service.py
+++ iterate_service.py
@@ -11,6 +11,8 @@ import argparse
 import json
 import logging
 import socket
+import requests
+import threading
 
 # import project libs
 
@@ -21,6 +23,7 @@ import merge_processing
 # defining globals & constants
 
 global app
+global args
 app = Flask(__name__)
 
 # Flask routes
@@ -41,7 +44,7 @@ def who_are_you():
         ]
     }
 
-    return create_json_respons_from(message)
+    return create_json_response_from(message)
 
 @app.route('/iterate', methods=['GET'])
 def iterate_who_are_you():
@@ -53,18 +56,30 @@ def iterate_who_are_you():
         'problem_id': 'ner',
         'interface_types': [ 'ner_complete', 'ner_paragraph' ]
     }
-    return create_json_respons_from(message)
+    return create_json_response_from(message)
 
 @app.route('/iterate', methods=['POST'])
 def iterate():
     logging.info('iterate request')
-    corpus = iteration_processing.decode_post_data(request.json)
-    documents = iteration_processing.iterate_corpus(corpus)
+    if args.async:
+        logging.info('process asynchronously')
+        def async_processing(data):
+            corpus = iteration_processing.decode_post_data(data['raw_data'])
+            documents = iteration_processing.iterate_corpus(corpus)
+            response = { 'annotation_documents': documents }
+            res = requests.post(data['callback_url'], data=json.dumps(response), headers={ 'Content-Type': 'application/json' })
+        data = request.json
+        threading.Thread(target=async_processing, args=(data,)).start()
+        return create_json_response_from({ 'status': 'async' })
+    else:
+        logging.info('process during request')
+        corpus = iteration_processing.decode_post_data(request.json['raw_data'])
+        documents = iteration_processing.iterate_corpus(corpus)
 
-    logging.info('transmitted corpus contains %s documents; created %s annotation documents' % (len(corpus), len(documents)))
+        logging.info('transmitted corpus contains %s documents; created %s annotation documents' % (len(corpus), len(documents)))
 
-    response = { 'annotation_documents': documents }
-    return create_json_respons_from(response)
+        response = { 'annotation_documents': documents }
+        return create_json_response_from(response)
 
 @app.route('/merge', methods=['GET'])
 def merge_who_are_you():
@@ -76,7 +91,7 @@ def merge_who_are_you():
         'version': 0.1,
         'problem_id': 'ner'
     }
-    return create_json_respons_from(message)
+    return create_json_response_from(message)
 
 @app.route('/merge', methods=['POST'])
 def merge():
@@ -85,11 +100,11 @@ def merge():
     logging.info('received %s documents as parts of raw datum #%s' % (len(annotation_documents), raw_datum_id))
 
     raw_datum = merge_processing.create_new_raw_datum(raw_datum_id, annotation_documents)
-    return create_json_respons_from(raw_datum)
+    return create_json_response_from(raw_datum)
 
 # helpers
 
-def create_json_respons_from(hash):
+def create_json_response_from(hash):
     response = jsonify(hash)
     response.status_code = 200
     return response
@@ -97,7 +112,6 @@ def create_json_respons_from(hash):
 # entry point as a stand alone script
 
 if __name__ == '__main__':
-    beVerbose = False
     usePort = 5001
     useHost = 'localhost'
     parser = argparse.ArgumentParser(
@@ -126,6 +140,12 @@ if __name__ == '__main__':
         action='store_true',
         dest='verbose',
         help='enables verbose mode')
+    parser.add_argument(
+        '-a',
+        '--async',
+        action='store_true',
+        dest='async',
+        help='communicates asynchronous and non-blocking with DALPHI')
     args = parser.parse_args()
 
     if args.port:
@@ -133,11 +153,9 @@ if __name__ == '__main__':
     if not args.localhost:
         hostename = socket.gethostname()
         useHost = socket.gethostbyname(hostename)
-    if args.verbose:
-        beVerbose = True
 
     logging.basicConfig(filename='service.log', level=logging.INFO)
     logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     logging.getLogger().addHandler(logging.StreamHandler())
     logging.info('start running flask app')
-    app.run(useHost, usePort, beVerbose)
+    app.run(useHost, usePort, args.verbose)
